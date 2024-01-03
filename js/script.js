f7ce0f3554cf1ca6261db03021a31bc606409cb2
moses <moses.mwangi.me@gmail.com>
2024-01-03 08:12:32 +0300

    comm

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 "use strict";
 
-const dice = Math.trunc(Math.random() * 8) + 1;
-while (h <= 6) {
-  console.log(` i roll ${dice}`);
-  const dice = Math.trunc(Math.random() * 8) + 1;
-}
+// const dice = Math.trunc(Math.random() * 8) + 1;
+// while (h <= 6) {
+//   console.log(` i roll ${dice}`);
+//   const dice = Math.trunc(Math.random() * 8) + 1;
+// }
