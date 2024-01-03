b58a47a7c656a61c873ec785d43bc949181641b9
moses <moses.mwangi.me@gmail.com>
2024-01-03 05:30:41 +0300

    commit

---
+++ js/script.js
@@ -0,0 +1,151 @@
+"use strict";
+// const firstName = "moses";
+// const lastName = "mwangi";
+
+// const name = "im" + firstName + lastName;
+// console.log(name);
+// const nameM = `im a ${firstName} ${lastName}`;
+// console.log(nameM);
+
+// const age = 19;
+// const old = age >= 18;
+// if (old) {
+//   console.log("she can drive");
+// } else {
+//   const wait = 18 - age;
+//   console.log(`she cant drive wait ${wait} years`);
+// }
+// const fav = number(prompt("whats your name"));
+// if (fav === 20) {
+//   console.log("correct");
+// } else if (fav === 7) {
+//   console.log("7 is a cool number");
+// } else {
+//   console.log("woow wrong  number");
+// }
+
+// const hasDriveLicence = true;
+// const hasGoodVision = false;
+
+// if (hasDriveLicence && hasGoodVision) {
+//   console.log("should drive");
+// } else {
+//   console.log("shouldnt drive");
+// }
+
+// const day = "monday";
+// switch (day) {
+//   case "monday":
+//     console.log("working from home");
+//     break;
+//   case "tusday":
+//     console.log("working from work");
+//     break;
+//   default:
+//     console.log("Not valid");
+// }
+
+// const whichDay = "monday";
+// const which = whichDay ? "going to church" : "working from home";
+// console.log(which);
+
+// function logg() {
+//   console.log("moses mwangi is a good man");
+// }
+// logg();
+
+// function fruit(apple, mango) {
+//   console.log(apple, mango);
+//   const juice = `juice made by just a ${apple} and ${mango}`;
+//   return juice;
+// }
+// console.log(fruit(3, 8));
+
+// // const fruit = function (apple, mango) {
+// //   console.log(apple, mango);
+// //   const juice = `juice made by just a ${apple} and ${mango}`;
+// //   return juice;
+// // };
+// // console.log(fruit(3, 8));
+
+// const cutFruit = function (fruit) {
+//   return fruit * 4;
+// };
+// const fruitProcessor = (apple, mango) => {
+//   const fruitApple = cutFruit(apple);
+//   const fruitMango = cutFruit(mango);
+//   const juice = `juice made by just a ${fruitApple} and ${fruitMango}`;
+//   return juice;
+// };
+// console.log(fruitProcessor(2, 5));
+
+// const friends = ["moses", "mwangi", "kimani"];
+// console.log(friends[0]);
+// console.log(friends.length);
+// friends[1] = "mungai";
+
+// const calcage = function (birthyear) {
+//   return birthyear - 2024;
+// };
+// const year = [1990, 2000, 2001];
+// const ages = calcage(year[(0, 1)]);
+
+// friends.push("kamaa");
+// friends.pop();
+
+// friends.unshift("jonh");
+// friends.shift();
+// console.log(friends.includes("mwangi"));
+/*
+const moses = {
+  firstName: "moses",
+  lastName: "mwangi",
+  age: 2023 - 2002,
+  friends: ["kamaa", "njoro", "bresh", "carlos"],
+};
+// const namekey = "Name";
+// console.log(moses.firstName);
+// console.log(moses["firstName"]);
+// console.log(moses["first" + namekey]);
+/*
+const intrestedIn = prompt("whats your fav about moses");
+console.log(moses[intrestedIn]);
+
+moses.location = "Nairobi Kenya";
+moses[G - mail] = "moses.mwangi";
+
+for (let rep = 1; rep <= 10; rep++) {
+  console.log(`i lift weght too much repeating ${rep}`);
+}
+
+const Name = ["moses", "mwangi", "kimani", "shiro"];
+const type = [];
+for (let i = 0; i < Name.lenght; i++) {
+  console.log(Name[i], typeof Name[i]);
+  // type[i] = typeof Name[1];
+  type.push(typeof Name);
+}
+console.log(type);
+
+const years = [1990, 1991, 1996, 1980, 2000];
+const ages = [];
+for (let ye = 0; ye < years.length; ye++) {
+  const year = 2037 - years[ye];
+  console.log(year);
+  ages.push(year);
+}
+
+let y = 1;
+while (y <= 6) {
+  console.log(`lifting wait ${y}`);
+  y--;
+}
+
+const dice = Math.trunc(Math.random() * 8) + 1;
+while (h <= 6) {
+  console.log(` i roll ${dice}`);
+  const dice = Math.trunc(Math.random() * 8) + 1;
+}*/
+////////////////////// working with arrays ///////////////////////
+const arr = ["a", "b", "c", "d", "e"];
+console.log(arr.slice());
