1dd9977bdcdecc06703e100af0b9c6e037dd583c
moses <moses.mwangi.me@gmail.com>
2024-01-06 10:14:53 +0300

    commit

--- js/script.js
+++ js/script.js
@@ -1,14 +1,47 @@
 "use strict";
 
-// Elements
+const account1 = {
+  owner: "moses mwangi",
+  movements: [200, 455.23, -306.5, 25000, -642.21, -133.9, 79.97, 1300],
+  interestRate: 1.2, // %
+  pin: 1111,
 
-// const movementDep = document.querySelector(
-//   ".movements__type movements__type--deposit"
-// );
-// const movementWith = document.querySelector(
-//   ".movements__type movements__type--withdrawal"
-// );
-// const movementValue = document.querySelector(".movementValue");
+  movementsDates: [
+    "2019-11-18T21:31:17.178Z",
+    "2019-12-23T07:42:02.383Z",
+    "2020-01-28T09:15:04.904Z",
+    "2020-04-01T10:17:24.185Z",
+    "2020-05-08T14:11:59.604Z",
+    "2020-05-27T17:01:17.194Z",
+    "2020-07-11T23:36:17.929Z",
+    "2020-07-12T10:51:36.790Z",
+  ],
+  currency: "EUR",
+  locale: "pt-PT", // de-DE
+};
+
+const account2 = {
+  owner: "Jessica Davis",
+  movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
+  interestRate: 1.5,
+  pin: 2222,
+
+  movementsDates: [
+    "2019-11-01T13:15:33.035Z",
+    "2019-11-30T09:48:16.867Z",
+    "2019-12-25T06:04:23.907Z",
+    "2020-01-25T14:18:46.235Z",
+    "2020-02-05T16:33:06.386Z",
+    "2020-04-10T14:43:26.374Z",
+    "2020-06-25T18:49:59.371Z",
+    "2020-07-26T12:01:20.894Z",
+  ],
+  currency: "USD",
+  locale: "en-US",
+};
+
+const accounts = [account1, account2];
+// Elements
 
 const labelWelcome = document.querySelector(".welcome");
 const labelDate = document.querySelector(".date");
@@ -35,7 +68,7 @@ const inputLoanAmount = document.querySelector(".form__input--loan-amount");
 const inputCloseUsername = document.querySelector(".form__input--user");
 const inputClosePin = document.querySelector(".form__input--pin");
 
-const account1 = {
+/*const account1 = {
   owner: "moses mwangi",
   movements: [200, -400, 3000, -650, -130, 70, 1300, 900, -870],
   interestRate: 1.2,
@@ -62,7 +95,7 @@ const account4 = {
   interestRate: 1,
   pin: 44,
 };
-const accounts = [account1, account2, account3, account4];
+const accounts = [account1, account2, account3, account4];*/
 
 const displayMovements = function (movements, sort = false) {
   containerMovements.innerHTML = "";
@@ -407,15 +440,16 @@ arr.fill(2, 4, 6);
 console.log(arr, arr2);
 
 const arr3 = Array.from({ length: 8 }, () => 1);
-const arr4 = Array.from({ length: 8 }, (_, i) => i + 1);
+const arr4 = Array.from({ length: 8 }, (cur, i) => i + 1);
 
 console.log(arr3, arr4);
 
 labelBalance.addEventListener("click", function () {
   const movementsUI = Array.from(document.querySelector(".movements__value"));
   console.log(movementsUI);
-});*/
-
+});
+*/
+/*
 //(1)
 const bankDepositSum = accounts
   .flatMap((acc) => acc.movements)
@@ -459,3 +493,16 @@ const capital = function (title) {
   return cap;
 };
 console.log(capital("this is a nice guy and GOOD buT HES dirty"));
+*/
+console.log(Number.parseInt("30px", 10));
+console.log(Number.parseInt("3.55px"));
+console.log(Number.parseFloat("30px"));
+console.log(Number.parseFloat("3.55px"));
+const movements = [200, 20, -400, -150, 340, 300, -20, 50, 400, -460];
+const v = movements.reduce((acc, cur) => (cur > 0 ? (acc += cur) : acc), 0);
+const z = movements.sort((a, b) => a - b);
+const d = movements.reduce((count, cur, i) => (count += 1), 0);
+console.log(v);
+// console.log(w);
+console.log(z);
+console.log(d);
